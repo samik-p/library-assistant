ee073e8851e68c67a562abae78f1b1f33ce6c918
Samik Pradhan <samikpradhan@gmail.com>
2023-08-20 18:37:15 -0700

    added inifinite loop to have a continous conversation

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ YELLOW = "\033[33m"
 BLUE = "\033[34m"
 
 if __name__ == "__main__":
-    input = speech_to_text()
-    output = get_response(input)
+    while True:
+        input = speech_to_text()
 
-    print(GREEN + "USER INPUT: " + RESET, end="")
-    print(input)
+        if "quit" in input:
+            break
 
-    print(BLUE + "RESPONSE: " + RESET, end="")
-    print(output)
+        output = get_response(input)
+
+        print(GREEN + "USER INPUT: " + RESET, end="")
+        print(input)
+
+        print(BLUE + "RESPONSE: " + RESET, end="")
+        print(output)
+
+    print(GREEN + "You have exited the program! Have a nice day :)" + RESET)
