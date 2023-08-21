2d4b6dec667a4cc96a84504c62be8b0b81a86e04
Samik Pradhan <samikpradhan@gmail.com>
2023-08-21 11:35:36 -0700

    update examples

--- classify.py
+++ classify.py
@@ -43,24 +43,3 @@ def classify_input(input_text, examples):
     # gets the prediction of the first classification object in response list
     # NOTE: only one object in list anyways
     return response[0].prediction
-
-
-# examples = [
-#     Example("Is Percy Jackson and the Lightning Thief available?", "See librarian"),
-#     Example("Can I check Gone with the Wind out?", "See librarian"),
-#     Example("I'm looking for some books about Australia.", "See librarian"),
-#     Example("What books can help me learn more about dinosaurs?", "See librarian"),
-#     Example("Are there any library programs happening today?", "See librarian"),
-#     Example("Is Magic Tree House #39 available at this library?", "See librarian"),
-#     Example("I need help checking into a computer.", "See librarian"),
-#     Example("What are the library's hours today?", "Answer directly"),
-#     Example("Can teens get volunteer hours here?", "Answer directly"),
-#     Example("Where in the library can I find the children's books?", "Answer directly"),
-#     Example("Is food allowed at the library?", "Answer directly"),
-#     Example("Are masks required to be worn at the library?", "Answer directly"),
-#     Example("How long until the library closes?", "Answer directly"),
-#     Example("Hello", "Conversation"),
-#     Example("Goodbye", "Conversation"),
-#     Example("How are you doing?", "Conversation"),
-#     Example("Who are you?", "Conversation"),
-# ]
