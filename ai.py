3ae5e7e98fe399a02eeb884347faef6a6bf12d33
Samik Pradhan <samikpradhan@gmail.com>
2023-08-20 18:54:32 -0700

    improved prompt generation

--- ai.py
+++ ai.py
@@ -8,11 +8,18 @@ load_dotenv()
 # authenticate
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+def preprocess_input(input_text):
+    prompt = f"""You are a library assistant. If the question relies on library-specific data, return 'FLAG'.
+Otherwise, answer the question based on context:
+{input_text}
+     
+If the input is not a valid question, return 'I don't understand'."""
+    return prompt
 
 def get_response(input_text):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=input_text,
+        prompt=preprocess_input(input_text),
         temperature=0.6,
         max_tokens=100,
     )
