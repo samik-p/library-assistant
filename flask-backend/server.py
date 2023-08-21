93712f5a3290c00149f2b49951814c19faefbfb9
Samik Pradhan <samikpradhan@gmail.com>
2023-08-21 14:20:07 -0700

    chatbot functionality with frontend

---
+++ flask-backend/server.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, make_response
+from flask_cors import CORS, cross_origin
+
+from ai import *
+
+app = Flask(__name__)
+CORS(app)
+
+# vars
+examples = parse_file_for_examples("examples.txt")
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+# request: {
+#   user_message: string
+# }
+@app.route("/chat", methods=["POST"])
+# @cross_origin(origins=["http://localhost:3000/"])
+def hello_chat():
+    # get request data, and then input text from data
+    # print("hello")
+    print("USER: " + request.json["user_message"])
+
+    data = request.json
+    input_text = data["user_message"]
+
+    # get response
+    response = get_response(input_text, examples)
+    print("RESPONSE: " + response)
+
+    # make http response using make_response
+    status_code = 200
+
+    http_response = make_response({"response": response}, status_code)
+    return http_response
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080", debug=True)
--- library-assistant-app/src/App.js
+++ library-assistant-app/src/App.js
@@ -1,39 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
+  Input,
+  Button,
   VStack,
-  Code,
-  Grid,
-  theme,
+  Text,
+  Divider,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (inputText.trim() !== '') {
+      // console.log("PRINT: " + get_assistant_response("ok"));
+      get_assistant_response(inputText);
+      setInputText('');
+    }
+  };
+
+  const get_assistant_response = (user_input) => {
+    fetch("http://localhost:8080/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ "user_message": user_input })
+    }).then((response) => {
+      // first then: checks for error
+      // console.log("First then")
+      // console.log(response)
+      if (!response.ok) {
+        throw new Error('HTTP error! Status: ' + response.status)
+      }
+      return response.json();
+    })
+      .then((data) => {
+        // console.log("Second then")
+        // process the JSON response data
+        console.log("RESPONSE: " + data["response"])
+        // return data["response"];
+        setMessages([...messages, { text: inputText, isUser: true }, { text: data["response"], isUser: false }])
+      })
+      .catch((error) => {
+        console.log("Error")
+        // handle errors
+        console.error('Fetch error:', error)
+      })
+  }
+
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+    <ChakraProvider>
+      <Box h="100vh" pt="100px" display="flex" alignItems="center" justifyContent="center">
+        <Box maxW="90vw" borderWidth="1px" borderRadius="lg" p="4">
+          <VStack spacing="4">
+            <Box flex="1">
+              <Text fontSize="lg" fontWeight="bold" align="center">
+                Chatbot
+              </Text>
+              <Divider mt="2" mb="4" />
+              <Box
+                height="80vh"  // Adjust the height as needed
+                width="80vw"
+                overflowY="scroll"
+                borderRadius="md"
+                p="2"
+                bg="gray.100"
+              >
+                {messages.map((message, index) => (
+                  <Text
+                    key={index}
+                    bg={message.isUser ? 'blue.200' : 'gray.200'}
+                    p="2"
+                    borderRadius="md"
+                    color="black"
+                    margin="0.5vh"
+                  >
+                    {message.text}
+                  </Text>
+                ))}
+              </Box>
+            </Box>
+            <Input
+              value={inputText}
+              onChange={handleInputChange}
+              placeholder="Type your message..."
+            />
+            <Button colorScheme="blue" onClick={handleSendMessage}>
+              Send
+            </Button>
           </VStack>
-        </Grid>
+        </Box>
       </Box>
     </ChakraProvider>
   );
