aadf53f21e3e861c35d86d3e0adc88addbc0b487
Samik Pradhan <samikpradhan@gmail.com>
2023-08-20 18:18:21 -0700

    LLM repsonse to user input implemented

--- ai.py
+++ ai.py
@@ -7,3 +7,14 @@ load_dotenv()
 
 # authenticate
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def get_response(input_text):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=input_text,
+        temperature=0.6,
+        max_tokens=100,
+    )
+
+    return response.choices[0].text.strip()
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from sr import *
 from ai import *
 
+# text colors (ANSI)
+RESET = "\033[0m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+
 if __name__ == "__main__":
-    print(speech_to_text())
+    input = speech_to_text()
+    output = get_response(input)
+
+    print(GREEN + "USER INPUT: " + RESET, end="")
+    print(input)
+
+    print(BLUE + "RESPONSE: " + RESET, end="")
+    print(output)
