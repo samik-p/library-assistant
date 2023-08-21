247889152f7a22e24e49b3011fe0fb7cf08e8637
Samik Pradhan <samikpradhan@gmail.com>
2023-08-20 19:21:18 -0700

    created a function that generates a report based on input

--- ai.py
+++ ai.py
@@ -8,7 +8,7 @@ load_dotenv()
 # authenticate
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def preprocess_input(input_text):
+def generate_scenario_prompt(input_text):
     prompt = f"""You are a library assistant. If the question relies on library-specific data, return 'FLAG'.
 Otherwise, answer the question based on context:
 {input_text}
@@ -19,9 +19,35 @@ If the input is not a valid question, return 'I don't understand'."""
 def get_response(input_text):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=preprocess_input(input_text),
+        prompt=generate_scenario_prompt(input_text),
         temperature=0.6,
         max_tokens=100,
     )
 
-    return response.choices[0].text.strip()
+    output_text = response.choices[0].text.strip()
+
+    if "FLAG" in output_text:
+
+        call_librarian(input_text)
+
+        return "Calling a librarian to help you."
+
+    return output_text
+
+def call_librarian(input_text):
+    
+    # call a librarian and
+    # send the input text for context
+
+    backup_prompt = f"A patron just asked: '{input_text}'. Summarize this question and generate a short report for the librarian."
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=backup_prompt,
+        temperature=0.6,
+        max_tokens=100,
+    )
+    message = response.choices[0].text.strip()
+
+    print(f"\n=========\nSENT TO LIBRARIAN: {message}\n=========")
+
