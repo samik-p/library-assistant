a01386571f5ec32b42b8bb494481703cbd2f4ca0
Samik Pradhan <samikpradhan@gmail.com>
2023-08-20 17:59:28 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from sr import *
+from ai import *
+
+if __name__ == "__main__":
+    print(speech_to_text())
---
+++ sr.py
@@ -0,0 +1,27 @@
+import os
+
+import speech_recognition as sr
+
+
+def get_voice_input(listener, microphone):
+    with microphone as source:
+        print("listening...")
+        # get user input (voice)
+        user_input = listener.listen(source)
+        # uses Google API
+        voice = listener.recognize_google(user_input)
+        # makes sure input string is lowercase
+        voice = voice.lower()
+    return voice
+
+
+def speech_to_text():
+    listener = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    input = get_voice_input(listener, microphone)
+    return input
+
+
+def translate_to_eng(input):
+    pass
